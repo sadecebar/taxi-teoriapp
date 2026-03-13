4595dddfb6de0c568f3bb20aa79c52502c00ba36
Baran <Baran.broo@gmail.com>
2026-03-14 00:13:21 +0100

    questions test3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { supabase } from "./supabase.js";
-import { QUESTIONS } from './questions.js';
+import { QUESTIONS as importedQuestions } from "./questions.js";
 
 function Logo({ size = 42 }) {
   return (
