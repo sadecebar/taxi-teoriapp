2fdeb99cedac9c4ca86366c8663092cf0cb30f3f
Baran <Baran.broo@gmail.com>
2026-03-20 14:09:37 +0100

    Add automatic build versioning

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 import { readFileSync } from 'fs'
 
 const pkg = JSON.parse(readFileSync('./package.json', 'utf8'))
+const buildNumber = process.env.VITE_BUILD_NUMBER
+const appVersion = buildNumber ? `${pkg.version}+${buildNumber}` : pkg.version
 
 export default defineConfig({
   plugins: [react()],
   base: '/taxi-teoriapp/',
   define: {
-    'import.meta.env.VITE_APP_VERSION': JSON.stringify(pkg.version),
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(appVersion),
   },
 })
